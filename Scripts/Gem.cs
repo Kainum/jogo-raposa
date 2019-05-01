78dd0361842ca8751fbd1aa9d1a04b24aa11925d
Kainum <36868250+Kainum@users.noreply.github.com>
2019-05-01 11:44:09 -0300

    Add files via upload

---
+++ Scripts/Gem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : Item {
+
+	protected override void ItemEffect (Player player, int value)
+	{
+		player.ModGems (value);
+	}
+}
